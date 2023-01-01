78e7c49d501c32b7f9fffaf0cecf67810d2eea82
Gideon Paitoo <gpaitoo1@gmail.com>
2023-01-01 00:05:05 +0000

    fine tuning gunicorn

--- static/js/chart_custom_style2.js
+++ static/js/chart_custom_style2.js
@@ -8,18 +8,6 @@
 -------------------------------------------------------------------*/
 
 var presets = window.chartColors;
-		var utils = Samples.utils;
-		var inputs = {
-			min: -50,
-			max: 50,
-			count: 10,
-			decimals: 2,
-			continuity: 1
-		};
-
-		function generateData(config) {
-			return utils.numbers(Chart.helpers.merge(inputs, config || {}));
-		}
 
 
 		new Chart('chart-2', {
@@ -64,13 +52,6 @@ var presets = window.chartColors;
 			});
 
 
-		function generateLabels(config) {
-			return utils.months(Chart.helpers.merge({
-				count: inputs.count,
-				section: 3
-			}, config || {}));
-		}
-
 		var options = {
 			maintainAspectRatio: false,
 			spanGaps: false,
@@ -94,51 +75,4 @@ var presets = window.chartColors;
 			}
 		};
 
-		[false, 'origin', 'start', 'end'].forEach(function(boundary, index) {
 
-			// reset the random seed to generate the same data for all charts
-			utils.srand(8);
-
-			new Chart('chart-' + index1, {
-				type: 'line',
-				data: {
-					labels: generateLabels(),
-					datasets: [{
-						backgroundColor: ['rgba(33, 150, 243, 0.3)'],
-						borderColor: ['rgba(33, 150, 243, 1)'],	
-						data: generateData(),
-						label: 'Progress chart',
-						fill: boundary
-					}]
-				},
-				options: Chart.helpers.merge(options, {
-					title: {
-						text: 'fill: ' + boundary,
-						display: false
-					}
-				})
-			});
-		});
-
-		// eslint-disable-next-line no-unused-vars
-		function toggleSmooth(btn) {
-			var value = btn.classList.toggle('btn-on');
-			Chart.helpers.each(Chart.instances, function(chart) {
-				chart.options.elements.line.tension = value ? 0.4 : 0.000001;
-				chart.update();
-			});
-		}
-
-		// eslint-disable-next-line no-unused-vars
-		function randomize() {
-			var seed = utils.rand();
-			Chart.helpers.each(Chart.instances, function(chart) {
-				utils.srand(seed);
-
-				chart.data.datasets.forEach(function(dataset) {
-					dataset.data = generateData();
-				});
-
-				chart.update();
-			});
-		}
